b8f29dea8de76bba660fe0f76846c80df7eb6db8
mrmchughes <mrmchughes27@gmail.com>
2021-05-17 17:18:20 -0400

    Done for the day

--- app.js
+++ app.js
@@ -1,120 +1,135 @@
-/* grab necessary elements */
+/*Code taken and modified from: Aravind at https://thecodingpie.com/post/how-to-create-a-countdown-timer-in-javascript*/
+
 const form = document.querySelector('.form');
-const timeInput = document.querySelector('timerSelect');
-const format = document.querySelector('.set-btn');
+
+const timeInput = document.querySelector("select[name='workoutTime']");
+/* the select[name=''] was what cracked it for me, enabled me to use the dropdown and it's values. Need to remember this! */
+
+const format = document.querySelector("select[name='format']");
+
+const setBtn = document.querySelector('.set-btn');
+
 const countDown = document.querySelector('.countdown');
+ 
 const stopBtn = document.querySelector('.stop-btn');
+
 const resetBtn = document.querySelector('.reset-btn');
-/* grab necessary elements ends */ 
 
-
-/* global variables and constants*/
 let countDownInterval;
+
 let secondsLeftms;
+
 let endTime;
+
 let stopBtnClicked = false;
-/* global variables ends */
 
-
-/* .stop-btn click listener */
 stopBtn.addEventListener('click', () => {
-    stopBtnClicked = !stopBtnClicked;
-
-    if (stopBtnClicked === true) {
-        stopBtn.innerHTML = 'Play';
-        resetBtn.disabled = false;
-        clearInterval(countDownInterval);
-    } else if (stopBtnClicked === false) {
-        stopBtn.innerHTML = 'STOP';
-        resetBtn.disabled = true;
-        endTime = secondsLeftms + Date.now();
-        countDownInterval = setInterval(() => {
-            setCountDown(endTime);
-        }, 1000);
-    }
-});
-/* .stop-btn click listener ends */
-
-
-/* .reset-btn click listener */
-resetBtn.addEventListener('click', () => {
-    resetCountDown();
-});
-/* .reset-btn click listener ends */
-
-
-/* .form submit listener */
-form.addEventListener('submit', (event) => {
-    // prevent the default page reloading
-    event.preventDefault();
   
-    // get the countdown time user typed
-    let countDownTime = timeInput.value;
-  
-    // check if it is not zero
-    if (countDownTime > 0) {
-      {// 1 minute = 60000 ms (4 zeros)
-        countDownTime = countDownTime * 60000;
-      } 
-  
-      // get current time in milliseconds
-      const now = Date.now();
-      // calculate the ending time
-      endTime = now + countDownTime;
-  
-      // activate the countdown at first
+  stopBtnClicked = !stopBtnClicked;
+
+  if (stopBtnClicked === true) {
+    
+    stopBtn.innerHTML = 'PLAY';
+   
+    resetBtn.disabled = false;
+    
+    clearInterval(countDownInterval);
+  } else if (stopBtnClicked === false) {
+    
+    stopBtn.innerHTML = 'STOP';
+    
+    resetBtn.disabled = true;
+    
+    endTime = secondsLeftms + Date.now();
+    
+    countDownInterval = setInterval(() => {
       setCountDown(endTime);
+    }, 1000);
+  }
+});
+
+resetBtn.addEventListener('click', () => {
+  resetCountDown();
+});
+
+form.addEventListener('submit', (event) => {
   
-      countDownInterval = setInterval(() => {
-        setCountDown(endTime);
-      }, 1000);
-  
-      // then disable the .set-btn
-      setBtn.disabled = true;
-      // then enable the .stop-btn
-      stopBtn.disabled = false;
+  event.preventDefault();
+
+  let countDownTime = timeInput.value;
+
+  if (countDownTime > 0) {
+    
+    if (format.value === 'hours') {
+      
+      countDownTime = countDownTime * 3600000;
+    } else if (format.value === 'minutes') {
+      
+      countDownTime = countDownTime * 60000;
+    } else if (format.value === 'seconds') {
+      
+      countDownTime = countDownTime * 1000;
     }
-  
-  });
-/* .form submit listener ends */
 
+    
+    const now = Date.now();
+    
+    endTime = now + countDownTime;
 
-/* setCountDown function */
+    setCountDown(endTime);
+
+    countDownInterval = setInterval(() => {
+      setCountDown(endTime);
+    }, 1000);
+
+    setBtn.disabled = true;
+    
+    stopBtn.disabled = false;
+  }
+
+});
+
 const setCountDown = (endTime) => {
-    secondsLeftms = endTime -Date.now();
-    const secondsLeft = Math.round(secondsLeftms / 1000);
+  
+  secondsLeftms = endTime - Date.now();
+  
+  const secondsLeft = Math.round(secondsLeftms / 1000);
 
-    let minutes = Math.floor(secondsLeft / 60);
-    let seconds = secondsLeft % 60;
+  let hours = Math.floor(secondsLeft / 3600);
+  let minutes = Math.floor(secondsLeft / 60) - (hours * 60);
+  let seconds = secondsLeft % 60;
 
-    if (minutes < 10) {
-        minutes = `0${minutes}`;
-    }
-    if (seconds < 10) {
-        seconds = `0${seconds}`;
-    }
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (seconds < 10) {
+    seconds = `0${seconds}`;
+  }
+ 
+  if (secondsLeft < 0) {
+    resetCountDown();
+    return;
+  }
 
-    if (secondsLeft < 0) {
-        resetCountDown();
-        return;
-    }
-
-    countDown.innerHTML = `${minutes} : ${seconds}`;
+  countDown.innerHTML = `${hours} : ${minutes} : ${seconds}`;
 
 };
-/* setCountDown function ends */
 
+const resetCountDown = () => {
+ 
+  clearInterval(countDownInterval);
+  
+  countDown.innerHTML = '00 : 00 : 00';
+  
+  stopBtnClicked = false;
+  
+  stopBtn.innerHTML = 'STOP';
 
-/* resetCountDown function */
-const resetCountDown = (endTime) => {
-    clearInterval(countDownInterval);
-    countDown.innerHTML = '00 : 00';
-    stopBtnClicked = false;
-    stopBtn.innerHTML = 'STOP';
+  setBtn.disabled = false;
 
-    setBtn.disabled = false;
-
-    stopBtn.disabled = true;
-    resetBtn.disabled = true;
-}
-/* resetCountDown function ends */
+  stopBtn.disabled = true;
+  resetBtn.disabled = true;
+};
